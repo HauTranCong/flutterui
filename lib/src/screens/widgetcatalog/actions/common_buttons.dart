001bf7a600fd9c29ff6303280554d6faa428b0c6
HauTranCong <haunek22@gmail.com>
2025-01-10 17:14:24 +0900

    Learn about floating action button

--- lib/src/screens/widgetcatalog/actions/common_buttons.dart
+++ lib/src/screens/widgetcatalog/actions/common_buttons.dart
@@ -58,66 +58,16 @@ class ButtonTypesGroup extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          ElevatedButton(
-            onPressed: onPressed,
-            child: const Text('Elevated'),
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.pressed)) {
-                    return Theme.of(context).colorScheme.primary.withOpacity(0.5);
-                  }
-                  return null; // Use the component's default.
-                },
-              ),
-              foregroundColor: WidgetStateProperty.all<Color>(Colors.white), // text color
-              padding: WidgetStateProperty.all<EdgeInsets>(
-                EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              ),
-              textStyle: WidgetStateProperty.all<TextStyle>(
-                TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-          FilledButton(
-            onPressed: onPressed,
-            child: const Text('Filled'),
-            style: FilledButton.styleFrom(
-              backgroundColor: Colors.green, // background color
-              foregroundColor: Colors.white, // text color
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              textStyle: TextStyle(fontSize: 16),
-            ),
-          ),
+          ElevatedButton(onPressed: onPressed, child: const Text('Elevated')),
+          SizedBox(height: 16),
+          FilledButton(onPressed: onPressed, child: const Text('Filled')),
+          SizedBox(height: 16),
           FilledButton.tonal(
-            onPressed: onPressed,
-            child: const Text('Filled Tonal'),
-            style: FilledButton.styleFrom(
-              backgroundColor: Colors.orange, // background color
-              foregroundColor: Colors.white, // text color
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              textStyle: TextStyle(fontSize: 16),
-            ),
-          ),
-          OutlinedButton(
-            onPressed: onPressed,
-            child: const Text('Outlined'),
-            style: OutlinedButton.styleFrom(
-              backgroundColor: Colors.red, // text color
-              side: BorderSide(color: Colors.red), // border color
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              textStyle: TextStyle(fontSize: 16),
-            ),
-          ),
-          TextButton(
-            onPressed: onPressed,
-            child: const Text('Text'),
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.purple, // text color
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              textStyle: TextStyle(fontSize: 16),
-            ),
-          ),
+              onPressed: onPressed, child: const Text('Filled Tonal')),
+          SizedBox(height: 16),
+          OutlinedButton(onPressed: onPressed, child: const Text('Outlined')),
+          SizedBox(height: 16),
+          TextButton(onPressed: onPressed, child: const Text('Text')),
         ],
       ),
     );
--- lib/src/screens/widgetcatalog/actions/floating_action_button.dart
+++ lib/src/screens/widgetcatalog/actions/floating_action_button.dart
@@ -1,11 +1,196 @@
 import 'package:flutter/material.dart';
 
-class FloatingActionButtonPage extends StatelessWidget {
+class FloatingActionButtonPage extends StatefulWidget {
+  const FloatingActionButtonPage({super.key});
+
+  @override
+  State<FloatingActionButtonPage> createState() =>
+      _FloatingActionButtonPageState();
+}
+
+class _FloatingActionButtonPageState
+    extends State<FloatingActionButtonPage> {
+  // The FAB's foregroundColor, backgroundColor, and shape
+  static const List<(Color?, Color? background, ShapeBorder?)> customizations =
+      <(Color?, Color?, ShapeBorder?)>[
+    (null, null, null), // The FAB uses its default for null parameters.
+    (null, Colors.green, null),
+    (Colors.white, Colors.green, null),
+    (Colors.white, Colors.green, CircleBorder()),
+  ];
+  int index = 0; // Selects the customization.
+
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    Widget titleBox(String title) {
+      return DecoratedBox(
+        decoration: BoxDecoration(
+          color: colorScheme.inverseSurface,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: Text(title,
+              style: TextStyle(color: colorScheme.onInverseSurface)),
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text('Floating Action Button')),
-      body: Center(child: Text('Floating Action Button Content')),
+      appBar: AppBar(
+        title: const Text('FloatingActionButton Sample'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+              'Clickable block containing an icon that keeps a key actions always in reach.',
+              style: TextStyle(fontSize: 18),
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Small'),
+                const SizedBox(width: 16),
+                // An example of the small floating action button.
+                //
+                // https://m3.material.io/components/floating-action-button/specs#669a1be8-7271-48cb-a74d-dd502d73bda4
+                FloatingActionButton.small(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Regular'),
+                const SizedBox(width: 16),
+                // An example of the regular floating action button.
+                //
+                // https://m3.material.io/components/floating-action-button/specs#71504201-7bd1-423d-8bb7-07e0291743e5
+                FloatingActionButton(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Large'),
+                const SizedBox(width: 16),
+                // An example of the large floating action button.
+                //
+                // https://m3.material.io/components/floating-action-button/specs#9d7d3d6a-bab7-47cb-be32-5596fbd660fe
+                FloatingActionButton.large(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Extended'),
+                const SizedBox(width: 16),
+                // An example of the extended floating action button.
+                //
+                // https://m3.material.io/components/extended-fab/specs#686cb8af-87c9-48e8-a3e1-db9da6f6c69b
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    // Add your onPressed code here!
+                  },
+                  label: const Text('Add'),
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // Surface color mapping.
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FloatingActionButton.large(
+                      foregroundColor: colorScheme.primary,
+                      backgroundColor: colorScheme.surface,
+                      onPressed: () {
+                        // Add your onPressed code here!
+                      },
+                      child: const Icon(Icons.edit_outlined),
+                    ),
+                    const SizedBox(height: 20),
+                    titleBox('Surface'),
+                  ],
+                ),
+                // Secondary color mapping.
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FloatingActionButton.large(
+                      foregroundColor: colorScheme.onSecondaryContainer,
+                      backgroundColor: colorScheme.secondaryContainer,
+                      onPressed: () {
+                        // Add your onPressed code here!
+                      },
+                      child: const Icon(Icons.edit_outlined),
+                    ),
+                    const SizedBox(height: 20),
+                    titleBox('Secondary'),
+                  ],
+                ),
+                // Tertiary color mapping.
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FloatingActionButton.large(
+                      foregroundColor: colorScheme.onTertiaryContainer,
+                      backgroundColor: colorScheme.tertiaryContainer,
+                      onPressed: () {
+                        // Add your onPressed code here!
+                      },
+                      child: const Icon(Icons.edit_outlined),
+                    ),
+                    const SizedBox(height: 20),
+                    titleBox('Tertiary'),
+                  ],
+                ),
+              ],
+            ),
+            // Add more widgets as needed
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            index = (index + 1) % customizations.length;
+          });
+        },
+        foregroundColor: customizations[index].$1,
+        backgroundColor: customizations[index].$2,
+        shape: customizations[index].$3,
+        child: const Icon(Icons.navigation),
+      ),
     );
   }
 }
