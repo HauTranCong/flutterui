90b6ef828d0f16d943da59a979529f44b0f2c7d8
HauTranCong <haunek22@gmail.com>
2025-01-13 12:41:09 +0900

    Practicing with segment button

--- lib/src/screens/widgetcatalog/actions/segment_button.dart
+++ lib/src/screens/widgetcatalog/actions/segment_button.dart
@@ -15,6 +15,10 @@ class SegmentButtonPage extends StatelessWidget {
             textBox('#', inputFontSize: 18, url: 'https://m3.material.io/components/segmented-buttons/overview'),
             SizedBox(height: 24),
             SingleChoice(),
+            SizedBox(height: 24),
+            MultipleChoice(),
+            SizedBox(height: 24),
+            SegmentedButtonDecoration(),
           ],
         ),
       ),
@@ -73,6 +77,108 @@ class _SingleChoiceState extends State<SingleChoice> {
           ),
         ],
       ),
+    );
+  }
+}
+
+enum Sizes { extraSmall, small, medium, large, extraLarge }
+
+class MultipleChoice extends StatefulWidget {
+  const MultipleChoice({super.key});
+
+  @override
+  State<MultipleChoice> createState() => _MultipleChoiceState();
+}
+
+class _MultipleChoiceState extends State<MultipleChoice> {
+  Set<Sizes> selection = <Sizes>{Sizes.large, Sizes.extraLarge};
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('Selected sizes: ${selection.map((e) => e.toString().split('.').last).join(', ')}', style: TextStyle(fontSize: 18)),
+        SizedBox(height: 24),
+        SegmentedButton<Sizes>(
+          segments: const <ButtonSegment<Sizes>>[
+            ButtonSegment<Sizes>(value: Sizes.extraSmall, label: Text('XS')),
+            ButtonSegment<Sizes>(value: Sizes.small, label: Text('S')),
+            ButtonSegment<Sizes>(value: Sizes.medium, label: Text('M')),
+            ButtonSegment<Sizes>(
+              value: Sizes.large,
+              label: Text('L'),
+            ),
+            ButtonSegment<Sizes>(value: Sizes.extraLarge, label: Text('XL')),
+          ],
+          selected: selection,
+          onSelectionChanged: (Set<Sizes> newSelection) {
+            setState(() {
+              selection = newSelection;
+            });
+          },
+          multiSelectionEnabled: true,
+        ),
+      ],
+    );
+  }
+}
+
+class SegmentedButtonDecoration extends StatefulWidget {
+  const SegmentedButtonDecoration({super.key});
+
+  @override
+  State<SegmentedButtonDecoration> createState() => _SegmentedButtonDecorationState();
+}
+
+enum CalendarDecoration { day, week, month, year }
+
+class _SegmentedButtonDecorationState extends State<SegmentedButtonDecoration> {
+  CalendarDecoration calendarView = CalendarDecoration.week;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('Selected view: ${calendarView.toString().split('.').last}', style: TextStyle(fontSize: 18)),
+        SizedBox(height: 24),
+        SegmentedButton<CalendarDecoration>(
+          style: SegmentedButton.styleFrom(
+            backgroundColor: Colors.grey[200],
+            foregroundColor: Colors.red,
+            selectedForegroundColor: Colors.white,
+            selectedBackgroundColor: Colors.green,
+          ),
+          segments: const <ButtonSegment<CalendarDecoration>>[
+            ButtonSegment<CalendarDecoration>(
+              value: CalendarDecoration.day,
+              label: Text('Day'),
+              icon: Icon(Icons.calendar_view_day)),
+            ButtonSegment<CalendarDecoration>(
+              value: CalendarDecoration.week,
+              label: Text('Week'),
+              icon: Icon(Icons.calendar_view_week)),
+            ButtonSegment<CalendarDecoration>(
+              value: CalendarDecoration.month,
+              label: Text('Month'),
+              icon: Icon(Icons.calendar_view_month)),
+            ButtonSegment<CalendarDecoration>(
+              value: CalendarDecoration.year,
+              label: Text('Year'),
+              icon: Icon(Icons.calendar_today)),
+          ],
+          selected: <CalendarDecoration>{calendarView},
+          onSelectionChanged: (Set<CalendarDecoration> newSelection) {
+            setState(() {
+              // By default there is only a single segment that can be
+              // selected at one time, so its value is always the first
+              // item in the selected set.
+              calendarView = newSelection.first;
+            });
+          },
+        ),
+      ],
     );
   }
 }
