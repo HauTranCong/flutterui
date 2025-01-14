a193823396d13eeb33f0ed24b72a94346d224b8f
HauTranCong <haunek22@gmail.com>
2025-01-14 17:05:49 +0900

    Practicing with Alert Dialog (mini box and full box)

--- lib/src/screens/widgetcatalog/containment/alertdialog.dart
+++ lib/src/screens/widgetcatalog/containment/alertdialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterui/src/widgets/textbox.dart';
 
 class AlertDialogScreen extends StatelessWidget {
     @override
@@ -7,22 +8,90 @@ class AlertDialogScreen extends StatelessWidget {
             appBar: AppBar(
                 title: Text('Alert Dialog'),
             ),
-            body: ListView(
-                children: <Widget>[
-                    Column(
-                        children: <Widget>[
-                            ListTile(
-                                title: Text('Item 1'),
+            body: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                    children: <Widget>[
+                        textBox('Dialogs provide important prompts in a user flow.', inputFontSize: 20),
+                        textBox('#', inputFontSize: 20, url: 'https://m3.material.io/components/dialogs/overview'),
+                        DialogExample(),
+                        SizedBox(height: 16),
+                        FullScreenDialogExample(),
+                    ],
+                ),
+            ),
+        );
+    }
+}
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.blueAccent,
+      ),
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text(
+        'Show Dialog',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
+
+class FullScreenDialogExample extends StatelessWidget {
+    const FullScreenDialogExample({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+            ),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                    builder: (BuildContext context) => Scaffold(
+                        appBar: AppBar(
+                            title: const Text('Full Screen Dialog'),
+                        ),
+                        body: Center(
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                    const Text('This is a full screen dialog'),
+                                    TextButton(
+                                        onPressed: () => Navigator.pop(context),
+                                        child: const Text('Close Dialog'),
+                                    ),
+                                ],
                             ),
-                            ListTile(
-                                title: Text('Item 2'),
-                            ),
-                            ListTile(
-                                title: Text('Item 3'),
-                            ),
-                        ],
+                        ),
                     ),
-                ],
+                    fullscreenDialog: true,
+                ),
+            ),
+            child: const Text(
+                'Show Full Screen Dialog',
+                style: TextStyle(color: Colors.white),
             ),
         );
     }
