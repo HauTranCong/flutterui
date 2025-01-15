9be3e92f64fecab82b5683d9f217cfbc4e2fe00d
HauTranCong <haunek22@gmail.com>
2025-01-15 12:20:57 +0900

    Praticing with Cards

--- lib/src/screens/widgetcatalog/containment/card.dart
+++ lib/src/screens/widgetcatalog/containment/card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterui/src/widgets/textbox.dart';
 
 class CardScreen extends StatelessWidget {
     @override
@@ -7,20 +8,110 @@ class CardScreen extends StatelessWidget {
             appBar: AppBar(
                 title: Text('Card'),
             ),
-            body: ListView(
-                children: [
-                    Column(
-                        children: [
-                            TextField(
-                                decoration: InputDecoration(
-                                    labelText: 'Enter text',
-                                    border: OutlineInputBorder(),
-                                ),
-                            ),
-                        ],
-                    ),
-                ],
+            body: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                    children: [
+                        textBox(
+                            'Cards display content and actions about a single subject.',
+                            inputFontSize: 24,
+                        ),
+                        textBox(
+                            '#',
+                            inputFontSize: 24,
+                            url: 'https://m3.material.io/components/cards/overview',
+                        ),
+                        MusicCard(),
+                        SizedBox(height: 16),   
+                        TapCard(),
+                        SizedBox(height: 16),   
+                        Card(child: SampleCard(cardName: 'Elevated Card')),
+                        SizedBox(height: 16),   
+                        Card.filled(child: SampleCard(cardName: 'Filled Card')),
+                        SizedBox(height: 16),   
+                        Card.outlined(child: SampleCard(cardName: 'Outlined Card')),
+                    ],
+                ),
             ),
         );
     }
+}
+
+class MusicCard extends StatelessWidget {
+  const MusicCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TapCard extends StatelessWidget {
+  const TapCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        // clipBehavior is necessary because, without it, the InkWell's animation
+        // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+        // This comes with a small performance cost, and you should not set [clipBehavior]
+        // unless you need it.
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: const SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: Text('A card that can be tapped'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SampleCard extends StatelessWidget {
+  const SampleCard({required this.cardName});
+  final String cardName;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 100,
+      child: Center(child: Text(cardName)),
+    );
+  }
 }
--- lib/src/widgets/textbox.dart
+++ lib/src/widgets/textbox.dart
@@ -2,37 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-Widget textBox(String inputText, {double inputFontSize = 18, String? url}) {
-    return Align(
-        alignment: Alignment.centerLeft,
-        child: SelectableText.rich(
-            TextSpan(
-            style: TextStyle(fontSize: inputFontSize, color: Colors.black),
-            children: [
+Widget textBox(String inputText, {double inputFontSize = 18, String? url, double padding = 8}) {
+    return Padding(
+        padding: EdgeInsets.all(padding),
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: SelectableText.rich(
                 TextSpan(
-                    text: inputText,
-                    style: TextStyle(fontSize: inputFontSize),
-                ),
-                if (url != null) ...[
+                style: TextStyle(fontSize: inputFontSize, color: Colors.black),
+                children: [
                     TextSpan(
-                        text: ' ',
+                        text: inputText,
                         style: TextStyle(fontSize: inputFontSize),
                     ),
-                    TextSpan(
-                        text: url,
-                        style: TextStyle(fontSize: inputFontSize, color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                        ..onTap = () async {
-                            if (await canLaunch(url)) {
-                            await launch(url);
-                            } else {
-                            throw 'Could not launch $url';
-                            }
-                        },
-                    ),
+                    if (url != null) ...[
+                        TextSpan(
+                            text: ' ',
+                            style: TextStyle(fontSize: inputFontSize),
+                        ),
+                        TextSpan(
+                            text: url,
+                            style: TextStyle(fontSize: inputFontSize, color: Colors.blue),
+                            recognizer: TapGestureRecognizer()
+                            ..onTap = () async {
+                                if (await canLaunch(url)) {
+                                await launch(url);
+                                } else {
+                                throw 'Could not launch $url';
+                                }
+                            },
+                        ),
+                    ],
                 ],
-            ],
-            ),
+            )),
         ),
     );
 }
